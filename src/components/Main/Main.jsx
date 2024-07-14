a54d719fb99b8d1954615c0318b5e5e720fc1da9
Upeka Diluksha <dilukshaupeka@gmail.com>
2024-07-14 09:17:14 +0530

    Initial commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,82 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useContext } from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+import { Context } from '../../context/context'
+
+const Main = () => {
+
+const {onSent,recentPrompt,showResult,loading,resultData,setInput, input} = useContext(Context)
+
+  return (
+    <div className='main'>
+        <div className="nav">
+            <p>Gemini</p>
+            <img src={assets.user_icon} alt="" />
+        </div>
+        <div className="main-container">
+
+        {!showResult
+
+        ?<>
+        <div className="greet">
+                <p><span>Hello, Diluksha</span></p>
+                <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Suggest an italian food recipe</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Create a workout plan</p>
+                    <img src={assets.bulb_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Plan a relaxing day</p>
+                    <img src={assets.message_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>How to overcome procrastination</p>
+                    <img src={assets.code_icon} alt="" />
+                </div>
+            </div>
+            </>
+            :<div className='result'>
+                <div className="result-title">
+                    <img src={assets.user_icon} alt="" />
+                    <p>{recentPrompt}</p>
+                </div>
+                <div className="result-data">
+                    <img src={assets.gemini_icon} alt="" />
+                    {loading
+                    ?<div className='loader'>
+                        <hr />
+                        <hr />
+                        <hr />
+                    </div>
+                    : <p dangerouslySetInnerHTML={{ __html: resultData }}></p>}
+                    
+                </div>
+            </div>
+            }
+
+            
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input onChange={(e) => setInput(e.target.value)} value = {input} type="text" placeholder='Enter prompt here' />
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        {input? <img onClick={() => onSent()} src={assets.send_icon} alt="" />: null}
+                    </div>
+                </div>
+                <p className="bottom-info">Powered by OpenAI</p>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Main
